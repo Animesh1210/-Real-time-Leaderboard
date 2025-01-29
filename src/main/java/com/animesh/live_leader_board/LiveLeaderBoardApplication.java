afdf37fa075286c7af35f968c23a0584c282bc47
Animesh <animeshsingh.mail@gmail.com>
2025-01-29 22:58:58 +0530

    Initial commit

---
+++ src/main/java/com/animesh/live_leader_board/LiveLeaderBoardApplication.java
@@ -0,0 +1,13 @@
+package com.animesh.live_leader_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLeaderBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveLeaderBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animesh/live_leader_board/LiveLeaderBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh.live_leader_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveLeaderBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
